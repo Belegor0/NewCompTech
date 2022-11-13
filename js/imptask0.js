f7f8350b5e04be2ac09e636fa96cfd6d3fd043c7
Belegor0 <grandstrike@mail.ru>
2022-11-13 17:29:48 +0500

    1

--- js/imptask0.js
+++ js/imptask0.js
@@ -1,4 +1,6 @@
-var doc=document.querySelector('link[rel="import"]').import;
-var text=doc.querySelector('.cont1');
-var clone=document.importNode(text.content, true);
-document.querySelector('#container1').appendChild(clone);
+function show1(){
+    var doc=document.querySelector('link[rel="import"]').import;
+    var text=doc.querySelector('.cont1');
+    var clone=document.importNode(text.content, true);
+    document.querySelector("#container1").appendChild(clone);
+}
