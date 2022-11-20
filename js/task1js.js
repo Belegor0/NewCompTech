cecc8705c8b0b7d49b99d144921933bef8319f30
Belegor0 <grandstrike@mail.ru>
2022-11-20 16:37:45 +0500

    1

---
+++ js/task1js.js
@@ -0,0 +1,49 @@
+var slides = document.querySelectorAll('#showContainer .imageContainer');
+var currentSlide = 0;
+var interval = 8000;
+var slideInterval = setInterval(nextSlide,interval);
+
+function nextSlide() {
+    goToSlide(currentSlide+1);
+}
+function previousSlide() {
+    goToSlide(currentSlide-1);
+}
+function goToSlide(n) {
+    slides[currentSlide].className = 'imageContainer';
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].className = 'imageContainer imageShow';
+}
+
+var playing = true;
+var pauseButton = document.getElementById('pause');
+function pauseSlideshow() {
+    pauseButton.innerHTML = 'Продолжить';
+    pauseButton.style.borderColor = '#FF0000'
+    playing = false;
+    clearInterval(slideInterval);
+}
+function playSlideshow() {
+    pauseButton.innerHTML = 'Пауза';
+    pauseButton.style.borderColor = '#008000'
+    playing = true;
+    slideInterval = setInterval(nextSlide,interval);
+}
+pauseButton.onclick = function() {
+    if(playing) {
+    pauseSlideshow();
+  } else {
+    playSlideshow();
+  }
+};
+
+var next = document.getElementById('next');
+var previous = document.getElementById('prev');
+next.onclick = function() {
+    pauseSlideshow();
+    nextSlide();
+};
+previous.onclick = function() {
+    pauseSlideshow();
+    previousSlide();
+};
