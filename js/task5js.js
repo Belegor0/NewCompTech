95056ee0de88f8b35085fa451544878bf2bbcaae
Belegor0 <grandstrike@mail.ru>
2022-12-12 16:46:38 +0500

    1

---
+++ js/task5js.js
@@ -0,0 +1,23 @@
+var conts = document.querySelectorAll('.cont');
+var rem = document.getElementById('rem');
+console.log('Hello')
+
+rem.onclick = function () {
+    var fst = document.getElementById('fst').value;
+    var snd = document.getElementById('snd').value;
+    if (isNaN(Number(fst)) || isNaN(Number(snd)))
+    {
+        alert('Некорректный ввод');
+    }
+    else {
+        fst = Number(fst);
+        snd = Number(snd);
+        if ((fst < 1 || fst > 3) || (snd < 1 || snd > 3))
+        {
+            alert('Неверные цифры');
+        }
+        else {
+            [conts[fst-1].className, conts[snd-1].className] = [conts[snd-1].className, conts[fst-1].className];
+        }
+    }
+}
